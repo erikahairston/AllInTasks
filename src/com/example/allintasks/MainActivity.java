314fc2e3995b7ed318cb45e12380fb726b981829
erikahairston <erikahairston@gmail.com>
2014-11-21 11:20:00 -0500

    Test Erika1

--- src/com/example/allintasks/MainActivity.java
+++ src/com/example/allintasks/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hey Asia");
+        System.out.println("Hey Erika");
     }
 
 
