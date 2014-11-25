8e21e6a78a9e22d418901c1102251596cd3f9974
erikahairston <erikahairston@gmail.com>
2014-11-25 14:22:06 -0500

    Member Tasks Test

--- src/com/example/allintasks/MemberTasks.java
+++ src/com/example/allintasks/MemberTasks.java
@@ -3,7 +3,12 @@ package com.example.allintasks;
 public class MemberTasks {
 
 	public static void main(String[] args) {
-		
+		String userInputNameofAttendee1 = "hi";
+		int countAttendees = 0;
+		for (int i= 0; i < countAttendees; i++){
+			System.out.print("Member Name: " + userInputNameofAttendee1);
+			//place to input name of 
+		}
 
 	}
 
