5bb7cfae3c7ece5cb281affca1d888908d289f26
erikahairston <erikahairston@gmail.com>
2014-11-25 14:34:32 -0500

    Member Tasks: New Tasks

--- src/com/example/allintasks/MemberTasks.java
+++ src/com/example/allintasks/MemberTasks.java
@@ -3,11 +3,20 @@ package com.example.allintasks;
 public class MemberTasks {
 
 	public static void main(String[] args) {
-		String userInputNameofAttendee1 = "hi";
+		boolean newTask= true;
+		String userInputNameofAttendee = "hi";
+		String userInputTask = "do this";
 		int countAttendees = 0;
+		
 		for (int i= 0; i < countAttendees; i++){
-			System.out.print("Member Name: " + userInputNameofAttendee1);
-			//place to input name of 
+			System.out.print("Member Name: " + userInputNameofAttendee);
+			//prints out what was inputed under Members method
+			while (newTask)
+			{
+				System.out.print("Task 1: checkbox" + userInputTask);
+				//goes into scanner userInputTask is a place where the user types in the tasks
+			}
+			
 		}
 
 	}
