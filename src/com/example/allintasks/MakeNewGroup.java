b94af7ecfe99ee4dfed4a02e331167526d50608a
Nubia Jackson <nubia.jackson@yale.edu>
2014-12-02 17:55:26 -0500

    make group edits

--- src/com/example/allintasks/MakeNewGroup.java
+++ src/com/example/allintasks/MakeNewGroup.java
@@ -2,4 +2,14 @@ package com.example.allintasks;
 
 public class MakeNewGroup {
 
+	public static void main(String args[]){
+		
+	}
+	
+	public static String makeNewGroup(String a, String b, String c, String d){
+		
+		
+		
+		return 
+	}
 }
