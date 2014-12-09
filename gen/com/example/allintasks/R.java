582165160f4d94deca2f25c3d142639fcd2f8bcb
Nubia Jackson <nubia.jackson@yale.edu>
2014-12-08 21:24:16 -0500

    MNG output

--- gen/com/example/allintasks/R.java
+++ gen/com/example/allintasks/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+<<<<<<< Updated upstream
         public static final int LinearLayout1=0x7f080011;
         public static final int SMSLabel=0x7f08000e;
         public static final int action_settings=0x7f080014;
@@ -46,6 +47,27 @@ public final class R {
         public static final int textView2=0x7f080007;
         public static final int textView3=0x7f080008;
         public static final int textView4=0x7f080009;
+=======
+        public static final int LinearLayout1=0x7f08000f;
+        public static final int SMSLabel=0x7f080003;
+        public static final int action_settings=0x7f080012;
+        public static final int attendees=0x7f08000a;
+        public static final int attendeesList=0x7f080011;
+        public static final int button=0x7f080005;
+        public static final int current_date=0x7f080008;
+        public static final int due_date=0x7f080009;
+        public static final int editContactName=0x7f080002;
+        public static final int editSMS=0x7f080004;
+        public static final int group_name=0x7f080007;
+        public static final int linearLayout1=0x7f080000;
+        public static final int makenewgroup=0x7f080010;
+        public static final int phoneNum=0x7f080001;
+        public static final int save_button=0x7f08000e;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f08000b;
+        public static final int textView3=0x7f08000c;
+        public static final int textView4=0x7f08000d;
+>>>>>>> Stashed changes
     }
     public static final class layout {
         public static final int home_screen=0x7f030000;
